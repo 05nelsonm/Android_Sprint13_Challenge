db481e6928d5e1553ce46238f91ecfd54f39fd76
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-01 11:46:10 -0400

    added okHttp logging to MakeupApi

--- app/src/main/java/com/lambdaschool/sprintchallenge13/retrofit/MakeupApi.kt
+++ app/src/main/java/com/lambdaschool/sprintchallenge13/retrofit/MakeupApi.kt
@@ -3,11 +3,14 @@ package com.lambdaschool.sprintchallenge13.retrofit
 import com.google.gson.Gson
 import com.lambdaschool.sprintchallenge13.model.Makeup
 import io.reactivex.Single
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface MakeupApi {
 
@@ -16,17 +19,29 @@ interface MakeupApi {
 
     class Factory {
         companion object {
-            private val BASE_URL = "http://makeup-api.herokuapp.com/api/v1/"
-            private val gson = Gson()
-        }
+            val BASE_URL = "http://makeup-api.herokuapp.com/api/v1/"
+            val gson = Gson()
+            val logger = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+                level = HttpLoggingInterceptor.Level.BODY
+            }
 
-        fun create(): MakeupApi {
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .retryOnConnectionFailure(false)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(15, TimeUnit.SECONDS)
                 .build()
-                .create(MakeupApi::class.java)
+
+            fun create(): MakeupApi {
+                return Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build()
+                    .create(MakeupApi::class.java)
+            }
         }
     }
 }
