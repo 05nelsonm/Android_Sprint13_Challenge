9cf9009c6e6f34cad3a1b08de772e697b3a2c775
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-01 11:22:55 -0400

    added dependencies and built out data class model for API

---
+++ app/src/main/java/com/lambdaschool/sprintchallenge13/model/MakeupApi.kt
@@ -0,0 +1,11 @@
+package com.lambdaschool.sprintchallenge13.model
+
+data class MakeupApi (
+    val id: Int,
+    val brand: String,
+    val name: String,
+    val price: String,
+    val image_link: String,
+    val rating: Double,
+    val api_featured_image: String
+)
